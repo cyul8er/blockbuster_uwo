b046820c4470dc36431c6e847a35ea039358a0d2
Claire <claireyu.cyu@gmail.com>
2026-08-03 20:04:39 -0400

    home page sec1+2 base done

--- temp-app/app/page.tsx
+++ temp-app/app/page.tsx
@@ -9,16 +9,32 @@ export default function Home() {
     <HeaderCD></HeaderCD>
 
     <section className="sec sec2">
-    <div className = "leftHome">
-        <div className = "panel1">
-          <Image
-            src="/placeholder/Timeline.jpg"
-            alt="timeline"
-            width="714"
-            height="402"
-          />
+    <div className="leftHome">
+        <div className="panel0">
+            <Image
+                src="/placeholder/Timeline.jpg"
+                alt="photo one"
+                width="500"
+                height="500"
+            />
         </div>
-        
+        <div className="vertText vertTextA">
+            <span>⋆꙳❅*‧ ‧*❆ ₊⋆</span>
+
+        </div>
+        <div className="vertText vertTextB">
+            <span>･*❆ ₊⋆</span>            
+            <span>⋆꙳❅*°⋆❆.ೃ࿔*:</span>
+
+        </div>
+        <div className="panel1">
+            <Image
+                src="/placeholder/Timeline.jpg"
+                alt="photo two"
+                width="500"
+                height="500"
+            />
+        </div>
     </div>
 
     <div className = "rightHome">
@@ -26,11 +42,16 @@ export default function Home() {
             <p className = "font-bold">EST. 2025</p> <br /><br /><br />
             <p>At Blockbuster, we're dedicated to giving you and your fellow students at Western the opportunity to become the best version of yourself through the beautiful collaborative art form of filmmaking. </p>
             <br />
-    <br /><br /><br /><br /><br />
+    <br /><br /><br />
         </div>
 
         <div className = "panel2">
-            {/* <img> */}
+          <Image
+                src="/placeholder/Timeline.jpg"
+                alt="photo three"
+                width="500"
+                height="500"
+            />
         </div>
     </div>
 
